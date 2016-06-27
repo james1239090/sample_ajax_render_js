3f8f96c18eda2fffd2bf579b1a186e117191bc2a
James <james1239090@gmail.com>
2016-06-27 18:15:22 +0800

    using ajax with new product

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 gem 'awesome_rails_console'
 gem 'bootstrap-sass'
-gem 'better_errors'
+
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
@@ -43,7 +43,8 @@ end
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
-
+  gem 'meta_request'
+  gem 'better_errors'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,41 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+
   end
   def new
     @product = Product.new
   end
   def create
+    @product = Product.new(product_params)
+    # @products = Product.all
+    respond_to do |format|
+      if @product.save
+        format.js { render 'create' , product: @product}
+        format.html {redirect_to products_path}
+      else
+        format.js { render 'create' , product: @product }
+      end
 
+    end
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :price)
   end
 end
